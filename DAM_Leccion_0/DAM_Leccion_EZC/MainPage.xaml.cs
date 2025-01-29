ded4bf1077c0c7f53244e989a718d0cecf10a2d2
Lalushe <21030196@itesa.edu.mx>
2025-01-28 19:25:39 -0600

    Version_03: Se hizo los dos dos tipos de bindings y el binding context

--- DAM_Leccion_0/DAM_Leccion_EZC/MainPage.xaml.cs
+++ DAM_Leccion_0/DAM_Leccion_EZC/MainPage.xaml.cs
@@ -26,12 +26,19 @@ namespace DAM_Leccion_EZC
 
         public void Ejecutar()
         {
-            PersonaModel personaModel = new PersonaModel();
+            PersonaModel personaModel = new PersonaModel()
+            {
+                Nombre = "Hola aqui estoy",
+            };
 
-            personaModel.Nombre = "Hola aqui estoy";
+            BindingContext = personaModel.Nombre;
 
+            //txtNombre.Text = personaModel.Nombre;
 
-            txtNombre.Text = personaModel.Nombre;
+            //Binding personaBinding = new Binding(); 
+            //personaBinding.Source = personaModel;   //Origen
+            //personaBinding.Path = "Nombre"; //Ruta
+            //txtNombre.SetBinding(Entry.TextProperty, personaBinding); //Destino
         }
 
         private void btnAceptar_Clicked(System.Object sender, System.EventArgs e)
