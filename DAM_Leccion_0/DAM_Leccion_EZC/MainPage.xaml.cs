b13e1b924e531381735372bf65d5a0e856e9785a
Lalushe <21030196@itesa.edu.mx>
2025-01-28 18:52:41 -0600

    Version_02: Instrucciones báscias  en botón y caja de texto

--- DAM_Leccion_0/DAM_Leccion_EZC/MainPage.xaml.cs
+++ DAM_Leccion_0/DAM_Leccion_EZC/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_EZC
+﻿using DAM_Leccion_EZC.Model;
+
+namespace DAM_Leccion_EZC
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,21 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+
+            personaModel.Nombre = "Hola aqui estoy";
+
+
+            txtNombre.Text = personaModel.Nombre;
+        }
+
+        private void btnAceptar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema", "Se ha guardado el registro en la BD", "Aceptar");
         }
     }
 
